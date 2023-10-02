60b0b6aa77fabe0d795f7bfb37984d49923ce7c1
Antonella Defilippi <antonelladefilipp14@gmail.com>
2023-10-02 01:17:51 -0300

    entrega

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer'
+import Navbar from './components/navbar/navbar'
+
+
+function App() {
+    return (
+        <>
+            <Navbar />
+            <ItemListContainer />
+        </>
+
+    );
+}
+export default App
