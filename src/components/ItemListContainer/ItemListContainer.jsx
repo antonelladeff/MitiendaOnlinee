5fae9c7d408c76d565174244cb1c4ff9bbfc9bfc
Antonella Defilippi <antonelladefilipp14@gmail.com>
2023-10-02 00:43:22 -0300

    trabajando para la primera entrega

---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Title from '../Title/title';
+
+export const ItemListContainer = (props) => {
+    return (
+        <Title greeting='Bienvenidos a Tienda Online' />
+    );
+};
+
+export default ItemListContainer;
